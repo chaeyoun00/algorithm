782bb98badba63ccc97a7bde1e0176898f018e87
leechaeyoun <chaeyoun00@yonsei.ac.kr>
2023-02-21 11:16:57 +0900

    [PGS] 개인정보 수집 유효기간 / LV1 /

---
+++ PGS_kakao2021_blind_1.py
@@ -0,0 +1,41 @@
+def solution(today, terms, privacies):
+    answer = []
+    
+    days = today.split(".")
+    term = {}
+    for i in terms:
+        month = i.split(" ")
+        term[month[0]] = month[1]
+    
+    for i in range(len(privacies)):
+        inform = privacies[i].split(" ")
+        day = inform[0].split(".")
+        
+        cal = term[inform[1]]
+        day[0] = str(int(day[0]) + (int(cal) // 12))
+        if int(day[1]) + (int(cal) % 12) > 12:
+            day[0] = str(int(day[0]) + 1)
+            day[1] = str(int(day[1]) + (int(cal) % 12) - 12)
+        else:
+            day[1] = str(int(day[1]) + (int(cal) % 12))
+        
+        if int(day[2]) - 1 == 0:
+            if int(day[1]) == 1:
+                day[0] = str(int(day[0]) - 1)
+                day[1] = '12'
+                day[2] = '28'
+            else: 
+                day[1] = str(int(day[1]) - 1)
+                day[2] = '28'
+        else:
+            day[2] = str(int(day[2]) - 1)
+            
+        if int(day[0]) < int(days[0]):
+            answer.append(i + 1)
+        elif int(day[0]) == int(days[0]):
+            if int(day[1]) < int(days[1]):
+                answer.append(i + 1)
+            elif int(day[1]) == int(days[1]):
+                if int(day[2]) < int(days[2]):
+                    answer.append(i + 1)
+    return answer
